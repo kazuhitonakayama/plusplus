8e0bf28eca63b25bc7a03a252be134a707a23601
中山一仁 <nakayamakazuhito@nakayamakazuhitos-MacBook-Air.local>
2023-09-24 15:06:24 +0900

    fix: herokuにデプロイするのでsocket modeをやめる

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const { App } = require('@slack/bolt');
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
+  socketMode: false,
   appToken: process.env.SLACK_APP_TOKEN,
   // ソケットモードではポートをリッスンしませんが、アプリを OAuth フローに対応させる場合、
   // 何らかのポートをリッスンする必要があります
