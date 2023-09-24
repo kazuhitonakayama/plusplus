2f55853cdbb89292268ef30cf8825fed71587b89
中山一仁 <nakayamakazuhito@nakayamakazuhitos-MacBook-Air.local>
2023-09-24 19:06:23 +0900

    add: `pg`にアクセスするための設定を追記

--- app.js
+++ app.js
@@ -1,4 +1,25 @@
 const { App } = require('@slack/bolt');
+// postgresqlの設定
+const { Client } = require('pg');
+const client = new Client({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+})
+
+.get('/db', async (req, res) => {
+  try {
+    const client = await client.connect();
+    const result = await client.query('SELECT * FROM points');
+    const results = { 'results': (result) ? result.rows : null};
+    res.render('pages/db', results );
+    client.release();
+  } catch (err) {
+    console.error(err);
+    res.send("Error " + err);
+  }
+})
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
@@ -16,6 +37,7 @@ app.message('++', async ({ message, say }) => {
   // message.textの内容において`@`から`|`までの文字列を取得する
   const regex = /<(.*?)>/g;
   const receivedUser = regex.exec(message.text)[1].replace('@', '');
+  const givedUser = message.user;
 
   await say(`<@${receivedUser}> get 56 points! thanks from <@${message.user}>!`);
 });
